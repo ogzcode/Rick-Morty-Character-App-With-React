56d896db79ce5bac57039c71fe0da82141f75e03
ogzCode <oguzguc44@gmail.com>
2022-10-20 18:10:44 +0300

    App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import data from "./json/data.json";
+import axios from 'axios';
+
+function Image(props){
+  return (
+    <img className='image' src={props.img}/>
+  );
+}
+
+function About(props){
+  return (
+    <div className='about__box'>
+      <p className='species'>Species <span>{props.species}</span></p>
+      <p className='gender'>Gender <span>{props.gender}</span></p>
+      <p className='location'>Location <span>{props.location}</span></p>
+    </div>
+  );
+}
+
+function CharacterCard(props){
+  return (
+    <div className='card'>
+      <Image img={props.character.image}/>
+      <p className='name'>{props.character.name}</p>
+      <About 
+        species={props.character.species} 
+        gender={props.character.gender} 
+        location={props.character.location.name}/>
+    </div>
+  );
+}
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  const client = axios.create({baseURL: "https://rickandmortyapi.com/api/character/?page=1"});
+
+  async function getCharacters(){
+    const response = await client.get();
+    console.log(response.data.results);
+    setCharacters(response.data.results);
+  }
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        characters.map((c, i) => <CharacterCard character={c} key={i}/>)
+      }
     </div>
   );
 }
