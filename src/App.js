c4de64ef0badbe753bf33aeef3ae937caad98442
ogzCode <oguzguc44@gmail.com>
2023-01-08 21:15:55 +0300

    Edited main component and added bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import './App.css';
-import {useState, useEffect} from "react";
-import data from "./json/data.json";
+import { useState, useEffect } from "react";
 import axios from 'axios';
-
-function Image(props){
-  return (
-    <img className='image' src={props.img}/>
-  );
-}
-
-function About(props){
-  return (
-    <div className='about__box'>
-      <p className='species'>Species <span>{props.species}</span></p>
-      <p className='gender'>Gender <span>{props.gender}</span></p>
-      <p className='location'>Location <span>{props.location}</span></p>
-    </div>
-  );
-}
-
-function CharacterCard(props){
-  return (
-    <div className='card'>
-      <Image img={props.character.image}/>
-      <p className='name'>{props.character.name}</p>
-      <About 
-        species={props.character.species} 
-        gender={props.character.gender} 
-        location={props.character.location.name}/>
-    </div>
-  );
-}
+import CharacterCard from "./components/CharacterCard";
 
 function App() {
   const [characters, setCharacters] = useState([]);
 
-  const client = axios.create({baseURL: "https://rickandmortyapi.com/api/character/?page=1"});
+  const client = axios.create({ baseURL: "https://rickandmortyapi.com/api/character/?page=1" });
 
-  async function getCharacters(){
+  async function getCharacters() {
     const response = await client.get();
-    console.log(response.data.results);
+    //console.log(response.data.results);
     setCharacters(response.data.results);
   }
 
@@ -47,10 +17,16 @@ function App() {
     getCharacters();
   }, []);
   return (
-    <div className="App">
-      {
-        characters.map((c, i) => <CharacterCard character={c} key={i}/>)
-      }
+    <div className="container-fluid bg-dark py-4">
+      <div className='row row-cols-1 row-cols-xl-2 d-flex justify-content-center g-5'>
+        {
+          characters.map((c, i) => (
+            <div className='col d-flex justify-content-center'>
+              <CharacterCard character={c} key={i} />
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
