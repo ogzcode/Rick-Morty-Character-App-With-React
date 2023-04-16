56a4fc6c93fb8b5ca1766c99869f3627f88678af
ogzCode <oguzguc44@gmail.com>
2023-04-16 09:56:18 +0300

    Route and redux store added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import { useSelector, useDispatch } from 'react-redux';
+import { getData, getDataWithURL } from './redux/data/dataSlice';
+import { useEffect } from 'react';
+import CharacterList from "./components/CharacterList";
+
+import NavBar from './components/NavBar';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
+  const url = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (url.pathname === "/all") {
+      dispatch(getDataWithURL(""));
+    }
+    else if (url.pathname === "/female") {
+      dispatch(getDataWithURL("/?gender=female"));
+    }
+    else if (url.pathname === "/male") {
+      dispatch(getDataWithURL("/?gender=male"));
+    }
+    else if (url.pathname === "/unknown") {
+      dispatch(getDataWithURL("/?status=unknown"));
+    }
+    else if (url.pathname === "/alive") {
+      dispatch(getDataWithURL("/?status=alive"));
+    }
+    else if (url.pathname === "/dead") {
+      dispatch(getDataWithURL("/?status=dead"));
+    }
+  }, [url, dispatch]);
+
   return (
     <div className="App">
-
+      <Header />
+      <NavBar />
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,47 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import CharacterList from './components/CharacterList'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/all",
+        element: <CharacterList/>
+      },
+      {
+        path: "/female",
+        element: <CharacterList/>
+      },
+      {
+        path: "/male",
+        element: <CharacterList/>
+      },
+      {
+        path: "/unknown",
+        element: <CharacterList/>
+      },
+      {
+        path: "/alive",
+        element: <CharacterList/>
+      },
+      {
+        path: "/dead",
+        element: <CharacterList/>
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 )
